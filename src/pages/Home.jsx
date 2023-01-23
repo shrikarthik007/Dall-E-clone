39ac615744c4c6cf29bde731349323261229f900
shrikarthik007 <holebagilshrikarthik@gmail.com>
2023-01-23 13:12:11 +0530

    comit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Loader, Card, FormField } from "../components";
 
 export const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return <div>Home</div>;
 };
