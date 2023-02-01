636994fff6f8cf92fc323a48221c8753870fed4f
shrikarthik007 <holebagilshrikarthik@gmail.com>
2023-02-01 23:12:30 +0530

    commit

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const FormField = () => {
-  return <div>FormField</div>;
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2"></div>
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,12 +44,9 @@ export const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3 ">
               {searchText ? (
-                <RenderCards
-                  data="searchedResults"
-                  title="No search results found"
-                />
+                <RenderCards data={[]} title="No search results found" />
               ) : (
-                <></>
+                <RenderCards data={[]} title="No posts found" />
               )}
             </div>
           </>
