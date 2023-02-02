a5ea03a060a7fcbc82f35abe52d649dddbf331b6
shrikarthik007 <holebagilshrikarthik@gmail.com>
2023-02-02 12:48:52 +0530

    commit

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -29,7 +29,7 @@ export const CreatePost = () => {
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
-            LabelName="Your name"
+            labelName="Your name"
             type="text"
             name="name"
             placeholder="John Doe"
@@ -37,7 +37,7 @@ export const CreatePost = () => {
             handleChange={handleChange}
           />
           <FormField
-            LabelName="Prompt"
+            labelName="Prompt"
             type="text"
             name="prompt"
             placeholder="A plush toy robot sitting against a yellow wall"
